327d8d2c4fee7a08f709cd138b05d5054adb015e
Anton <nokker@gmail.com>
2022-02-11 10:12:13 +0900

    initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import { defineComponent } from "vue";
+import TestPackage2 from "@/test-package-2.vue";
+
+export default defineComponent({
+    name: "ServeDev",
+    components: {
+        TestPackage2,
+    },
+});
+</script>
+
+<template>
+    <div id="app">
+        <test-package-2 />
+    </div>
+</template>
